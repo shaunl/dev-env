c53e0b7080dbbe9cda217da6bd223157a7058079
shaunl <shaun.lawless@gmail.com>
2014-02-03 22:07:26 +0000

    PHP module installed with packages: 'fpm', 'xdebug', 'curl', 'mysql', 'cli','mcrypt'

--- puppet/modules/devenv/manifests/init.pp
+++ puppet/modules/devenv/manifests/init.pp
@@ -1,5 +1,13 @@
-class devenv(){
+class devenv(
+  $php_modules     = ['fpm', 'xdebug', 'curl', 'mysql', 'cli','mcrypt']
+){
   class { 'apt':
     always_apt_update => false,
   }
+
+  class { 'php':
+    service => 'nginx'
+  }
+
+  php::module { $php_modules: }
 }
